729ce359568cbc7f85ab241d6dcd5354348477e4
inspekturkael <inspekturkael>
2022-07-08 23:51:09 +0700

    add allowed host

--- PortofolioOfMyself/settings.py
+++ PortofolioOfMyself/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ck##@4k^4uw+s@$a!o)l%82ii-y_9s0c8bmcxy-zumkc5cuumn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
